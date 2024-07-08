96d0a24718e7f0aea6f29277f5e544cbedb9c834
Atindra Mohan das <atindramohandas353@gmail.com>
2024-07-08 15:36:40 +0530

    changes cors

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,11 @@ connectDB();
 // };
 // app.use(cors(corsOptions));
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://cake-crm-frontend.vercel.app/"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 
 
